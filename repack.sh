fbe4e0dea2611fcf47e87c07a5d979dadd8efa33
AlexanderR <alexander.r@gmx.com>
2017-08-28 16:32:20 +0700

    Set up uploads

--- repack.sh
+++ repack.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
-rm *.aar &> /dev/null
-rm -r ./release-package/ &> /dev/null
+#!/bin/bash -e
+sha256sum lib/build/outputs/aar/lib-release.aar
+
+rm *.aar &> /dev/null || true
+rm -r ./release-package/ &> /dev/null || true
 
 pushd upx
 make all
@@ -16,15 +18,19 @@ popd
 
 unzip -o lib/build/outputs/aar/lib-release.aar -d release-package
 cd release-package
-zip --out classes-fixed.jar --fix classes.jar
+unzip -o classes.jar -d classes-repacked
+rm classes.jar
+pushd classes-repacked
+zip -X -D -Z store -R ../classes.jar "*"
+popd
+rm -r classes-repacked
 zip --out annotations-fixed.zip --fix annotations.zip
-mv classes-fixed.jar classes.jar
 mv annotations-fixed.zip annotations.zip
 ../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 classes.jar
 ../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 annotations.zip
 
 # non-compressed build
-zip -Z store -D -R ../release.aar "*"
+zip -X -D -Z store -R ../release.aar "*"
 advzip -z -4 ../release.aar
 ../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 ../release.aar
 sha256sum ../release.aar
@@ -33,7 +39,7 @@ sha256sum ../release.aar
 chmod +x jni/*/*.so
 ../upx/src/upx.out --best --ultra-brute --android-shlib jni/x86/*.so
 ../upx/src/upx.out --best --ultra-brute --android-shlib jni/armeabi-v7a/*.so
-zip -Z store -D -R ../release-compressed.aar "*"
+zip -X -D -Z store -R ../release-compressed.aar "*"
 advzip -z -4 ../release-compressed.aar
 ../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 ../release-compressed.aar
 sha256sum ../release-compressed.aar
