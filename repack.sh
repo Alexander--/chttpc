ef2e1badfc512efe780eb95088db923462024de8
AlexanderR <alexander.r@gmx.com>
2017-08-31 16:57:01 +0700

    Get rid of strip-nondeterminism

--- repack.sh
+++ repack.sh
@@ -1,7 +1,10 @@
 #!/bin/bash -ex
-sha256sum lib/build/outputs/aar/lib-release.aar
 
-STARTTIME=999974760
+export TZ=UTC
+export LC_ALL=C
+
+STARTTIME=1000000000
+STARTDATE='2001-09-09 01:46:00 i0,0'
 
 rm *.aar &> /dev/null || true
 rm -r ./release-package/ &> /dev/null || true
@@ -10,39 +13,38 @@ pushd upx
 make all
 popd
 
-pushd strip-nondeterminism
-mkdir -p ../tools
-perl Makefile.PL INSTALL_BASE=$(realpath ../tools)
-make
-make install
-export PERL5LIB=$(realpath ../tools/lib/perl5/)
-popd
-
 unzip -o lib/build/outputs/aar/lib-release.aar -d release-package
 cd release-package
+
 unzip -o classes.jar -d classes-repacked
 rm classes.jar
 pushd classes-repacked
-find . -type f | sort | fastjar -@c0M > ../classes.jar
+find . -type f | sort | faketime -f "$STARTDATE" fastjar -@c0M > ../classes.jar
 popd
 rm -r classes-repacked/
-zip --out annotations-fixed.zip --fix annotations.zip
-mv annotations-fixed.zip annotations.zip
-../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME classes.jar
-../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME annotations.zip
 
-# non-compressed build
-find . -type f | sort | fastjar -@c0M > ../release.aar
+unzip -o annotations.zip -d annotations-repacked
+rm annotations.zip
+pushd annotations-repacked
+find . -type f | sort | faketime -f "$STARTDATE" fastjar -@c0M > ../annotations.zip
+popd
+rm -r annotations-repacked/
+
+# no UPX
+find . -type f | sort | faketime -f "$STARTDATE" fastjar -@c0M > ../release.aar
 sha256sum ../release.aar
+
+# no UPX + advancecomp
 advzip -z -4 ../release.aar
-../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME ../release.aar
 sha256sum ../release.aar
 
-# UPX-compressed build
+# with UPX (should be repeatable, since we build UPX above, but who knows...)
 chmod +x jni/*/*.so
 ../upx/src/upx.out --best --ultra-brute --android-shlib jni/x86/*.so
 ../upx/src/upx.out --best --ultra-brute --android-shlib jni/armeabi-v7a/*.so
-find . -type f | sort | fastjar -@c0M > ../release-compressed.aar
+find . -type f | sort | faketime -f "$STARTDATE" fastjar -@c0M > ../release-compressed.aar
+sha256sum ../release-compressed.aar
+
+# UPX + advancecomp
 advzip -z -4 ../release-compressed.aar
-../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME ../release-compressed.aar
 sha256sum ../release-compressed.aar
