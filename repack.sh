f1dbf672e0fa7ebbba389c75f01ffdb8ed5cb95a
AlexanderR <alexander.r@gmx.com>
2017-08-31 08:54:11 +0700

    Fix repack

--- repack.sh
+++ repack.sh
@@ -1,5 +1,7 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 sha256sum lib/build/outputs/aar/lib-release.aar
+
+STARTTIME=999974760
 
 rm *.aar &> /dev/null || true
 rm -r ./release-package/ &> /dev/null || true
@@ -21,25 +23,26 @@ cd release-package
 unzip -o classes.jar -d classes-repacked
 rm classes.jar
 pushd classes-repacked
-zip -X -D -Z store -R ../classes.jar "*"
+find . -type f | sort | fastjar -@c0M > ../classes.jar
 popd
-rm -r classes-repacked
+rm -r classes-repacked/
 zip --out annotations-fixed.zip --fix annotations.zip
 mv annotations-fixed.zip annotations.zip
-../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 classes.jar
-../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 annotations.zip
+../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME classes.jar
+../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME annotations.zip
 
 # non-compressed build
-zip -X -D -Z store -R ../release.aar "*"
+find . -type f | sort | fastjar -@c0M > ../release.aar
+sha256sum ../release.aar
 advzip -z -4 ../release.aar
-../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 ../release.aar
+../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME ../release.aar
 sha256sum ../release.aar
 
 # UPX-compressed build
 chmod +x jni/*/*.so
 ../upx/src/upx.out --best --ultra-brute --android-shlib jni/x86/*.so
 ../upx/src/upx.out --best --ultra-brute --android-shlib jni/armeabi-v7a/*.so
-zip -X -D -Z store -R ../release-compressed.aar "*"
+find . -type f | sort | fastjar -@c0M > ../release-compressed.aar
 advzip -z -4 ../release-compressed.aar
-../tools/bin/strip-nondeterminism -v -t zip -T 1000000000 ../release-compressed.aar
+../tools/bin/strip-nondeterminism -v -t zip -T $STARTTIME ../release-compressed.aar
 sha256sum ../release-compressed.aar
